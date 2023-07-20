ef40a533829751f82e01638cac94889c22825940
Dorian Margraf <dorian.margraf@gmail.com>
2023-07-20 15:31:08 +0200

    Added EmployeeCard component nested in EmployeeOverview

---
+++ BethanysPieShop/Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShop.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShop.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+    }
+}
