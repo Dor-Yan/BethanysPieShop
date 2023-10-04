561449747a6560af8a9139c4f9d7b804eca1eb46
Dorian Margraf <dorian.margraf@gmail.com>
2023-10-04 15:21:03 +0200

    Added authentication with Auth0

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,7 @@
 using BethanysPieShop;
 using BethanysPieShop.Services;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,5 +18,11 @@ builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(
     => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped<ApplicationState>();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 await builder.Build().RunAsync();
